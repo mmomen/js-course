ee6313d9dcd3c1b306eaf3655fe15c4b0c99a270
Mazdak Momen <mazdak.momen@gmail.com>
2014-12-01 01:21:54 -0800

    Created onLoad, createPost, addComment and likePost functions

--- src/facebook.js
+++ src/facebook.js
@@ -7,6 +7,10 @@ var posts = [
     comments: [{
       comment: "hello world",
       id: 0
+    },
+    {
+      comment: "wuz up",
+      id: 1
     }]
   },
   {
@@ -18,13 +22,49 @@ var posts = [
   }
 ];
 
-var onLoad = function() {};
+var onLoad = function() {
+  posts.forEach(function(e) {
+    displayPost(e);
+    e.comments.reverse().forEach(function(element) {
+      displayComment(e.id,element);
+    });
+  });
+};
 
-var createPost = function(post) {};
+var createPost = function(post) {
+  var postsNum = $(".posts").find(".post").length;
+  var x = {
+    status: post,
+    likes: 0,
+    likedByMe: false,
+    id: postsNum,
+    comments: []
+  };
+  displayPost(x);
+  posts.push(x);
+};
 
-var likePost = function(postId) {};
+var likePost = function(postId) {
+  var $postToMod = $("#post" + postId).find(".like-count");
+  if (posts[postId].likedByMe) {
+    posts[postId].likedByMe = false;
+    posts[postId].likes--;
+  } else {
+    posts[postId].likedByMe = true;
+    posts[postId].likes++;
+  }
+  $postToMod.text("This has " + posts[postId].likes + " likes");
+};
 
-var addComment = function(postId, comment) {};
+var addComment = function(postId, comment) {
+  var commentId = $("#post" + postId).find(".comment").length;
+  var commentObj = {
+    comment: comment,
+    id: commentId
+  };
+  displayComment(postId, commentObj);
+  posts[postId].comments.push(commentObj);
+};
 
 var shortText = false;
 
